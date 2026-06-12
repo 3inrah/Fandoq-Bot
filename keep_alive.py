f9a33a8ba03aa97906fa29160fd6ec55147f796e
3inrah <saeedrahimi0909@gmail.com>
2026-06-12 21:32:41 +0330

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,1 @@
-from flask import Flask
-from threading import Thread
 
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "ربات فندق بیدار و در حال کار است!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
